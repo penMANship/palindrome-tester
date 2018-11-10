250034fc2b27a215d5dfeec308e55a9d9f70c6f8
Paul Nielsen <paul@thelanding.us>
2018-11-10 06:16:07 -0500

    no jekyll

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
 let Phrase = require("paunie-palindrome");
 
-alert(new Phrase("Madam, I'm Adam").palindrome());
+// alert(new Phrase("Madam, I'm Adam").palindrome());
+
+function palindromeTester() {
+    let string = prompt("Please enter a string for palindrome testing:");
+    let phrase = new Phrase(string);
+
+    if (phrase.palindrome()) {
+        alert(`"${phrase.content}" is a palindrome!`);
+    } else {
+        alert(`"${phrase.content}" is not a palindrome.`)
+    }
+}
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    let button = document.querySelector("#palindromeTester");
+    button.addEventListener("click", function() {
+        palindromeTester();
+    });
+});
